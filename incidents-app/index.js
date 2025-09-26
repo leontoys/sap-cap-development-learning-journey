0603a634f65698c0f115ec508e296669f20a8b76
leontoys <svliyon@gmail.com>
2025-09-27 07:35:36 +1000

    works locally

--- incidents-app/index.js
+++ incidents-app/index.js
@@ -1,10 +1,2 @@
 const cds = require("@sap/cds");
-
-// 1. CONFIGURE SQLite as the DB target
-cds.env.requires.db = { kind: "sqlite", impl: "@cap-js/sqlite" };
-
-// 2. DEPLOY to SQLite (creates tables + loads data)
-cds.deploy();
-
-// 3. START the server
 cds.server();
