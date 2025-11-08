43cbac0adccb0bacb29b150814fb9086b4319fcf
Liyon SV <svliyon@gmail.com>
2025-11-08 20:32:05 +0000

    episode13

--- hello-world/srv/services.js
+++ hello-world/srv/services.js
@@ -1,5 +1,15 @@
-module.exports = function (){
-  this.on ('READ',`Books`, (books)=>{
-    console.log("reached here")
-  })
+const cds = require('@sap/cds')
+class BooksService extends cds.ApplicationService{
+  init(){
+    const {Books} = this.entities
+
+    this.on('READ',Books, (req,next) => {
+      console.log("reached here")
+      return next() //you need to add return
+    })
+
+    return super.init()
+  }
 }
+
+module.exports = BooksService
