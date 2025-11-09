c5e697b47908538b0e15bd23ed52cba8b16dfe91
Liyon SV <svliyon@gmail.com>
2025-11-09 19:47:20 +0000

    part14

--- hello-world/srv/services.js
+++ hello-world/srv/services.js
@@ -1,13 +1,25 @@
 const cds = require('@sap/cds')
+
+//add logger 
+const logger = cds.log('cap-logger')
+//add this.after
+
 class BooksService extends cds.ApplicationService{
   init(){
     const {Books} = this.entities
 
-    this.on('READ',Books, (req,next) => {
-      console.log("reached here")
-      return next() //you need to add return
-    })
-
+//    this.on('READ',Books, (req,next) => {
+//      console.log("reached here")
+//      return next() //you need to add return
+//    })
+  logger('after reading')
+  this.after('READ',Books,(data,req)=>{
+    logger(data)
+    return data.map(book => book.title += '--!--')
+  })
+  this.after('each',Books,(data,req)=>{
+    logger(data)
+  })
     return super.init()
   }
 }
